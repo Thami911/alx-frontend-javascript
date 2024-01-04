d4505e921d5316bf12f267ca278738de7dd44574
Thami911 <122853011+Thami911@users.noreply.github.com>
2024-01-05 01:44:30 +0200

    Update

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,3 @@
 export default function getResponseFromAPI() {
-  let successful = true;
-  const promise = new Promise(((resolve, reject) => {
-    if (successful) {
-      resolve('Successful');
-    } else {
-      successful = false;
-      reject(Error('Unsuccessful'));
-    }
-  }));
-
-  return promise;
+  return new Promise(() => {});
 }
